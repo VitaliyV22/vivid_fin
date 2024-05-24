bdcc38554f7ad7854021f5f09b485cd6142bc353
VitaliyV22 <vitanskyyv@gmail.com>
2024-05-24 18:18:54 -0500

    added delete functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+from config import Config
 
 db = SQLAlchemy()
-migrate = Migrate()
 login = LoginManager()
-login.login_view = 'main.login'
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)
     login.init_app(app)
-
-    from . import routes, models
-    app.register_blueprint(routes.bp)
-
+    csrf.init_app(app)
+    
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -20,29 +20,29 @@ def index():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -50,7 +50,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
 
 @bp.route('/add_transaction', methods=['GET', 'POST'])
@@ -62,7 +62,7 @@ def add_transaction():
         db.session.add(transaction)
         db.session.commit()
         flash('Transaction added successfully')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template('add_transaction.html', title='Add Transaction', form=form)
 
 @bp.route('/add_budget', methods=['GET', 'POST'])
@@ -74,7 +74,7 @@ def add_budget():
         db.session.add(budget)
         db.session.commit()
         flash('Budget added successfully')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template('add_budget.html', title='Add Budget', form=form)
 
 @bp.route('/add_savings_goal', methods=['GET', 'POST'])
@@ -86,5 +86,41 @@ def add_savings_goal():
         db.session.add(savings_goal)
         db.session.commit()
         flash('Savings Goal added successfully')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template('add_savings_goal.html', title='Add Savings Goal', form=form)
+
+@bp.route('/delete_transaction/<int:transaction_id>', methods=['POST'])
+@login_required
+def delete_transaction(transaction_id):
+    transaction = Transaction.query.get_or_404(transaction_id)
+    if transaction.user_id != current_user.id:
+        flash('You do not have permission to delete this transaction.')
+        return redirect(url_for('main.index'))
+    db.session.delete(transaction)
+    db.session.commit()
+    flash('Transaction deleted successfully')
+    return redirect(url_for('main.index'))
+
+@bp.route('/delete_budget/<int:budget_id>', methods=['POST'])
+@login_required
+def delete_budget(budget_id):
+    budget = Budget.query.get_or_404(budget_id)
+    if budget.user_id != current_user.id:
+        flash('You do not have permission to delete this budget.')
+        return redirect(url_for('main.index'))
+    db.session.delete(budget)
+    db.session.commit()
+    flash('Budget deleted successfully')
+    return redirect(url_for('main.index'))
+
+@bp.route('/delete_savings_goal/<int:savings_goal_id>', methods=['POST'])
+@login_required
+def delete_savings_goal(savings_goal_id):
+    savings_goal = SavingsGoal.query.get_or_404(savings_goal_id)
+    if savings_goal.user_id != current_user.id:
+        flash('You do not have permission to delete this savings goal.')
+        return redirect(url_for('main.index'))
+    db.session.delete(savings_goal)
+    db.session.commit()
+    flash('Savings goal deleted successfully')
+    return redirect(url_for('main.index'))
