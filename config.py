136c093b4ac71a80b246e9bb3932c7f2b7bb0d8e
VitaliyV22 <vitanskyyv@gmail.com>
2024-11-02 00:41:13 -0500

    db update

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 
 load_dotenv()  # take environment variables from .env.
 
+print("SECRET_KEY:", os.environ.get('SECRET_KEY'))  # Debug print
+print("SQLALCHEMY_DATABASE_URI:", os.environ.get('SQLALCHEMY_DATABASE_URI'))  # Debug print
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
